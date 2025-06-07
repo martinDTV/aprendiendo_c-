c41f1d28824d52eb4c316958e8934c52bf7970d6
martinDTV <martin.delatorre@monterrey.gob.mx>
2025-06-06 22:02:05 -0600

    Nuevos cambios

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Estructuras de datos dinámicas: List<T> y Dictionary<TKey, TValue>
+
+Estas estructuras son fundamentales cuando necesitas trabajar con colecciones dinámicas y más potentes que los arrays.
+
+*/
+
+using System.Collections.Generic;
+
+List<string> tareas = new List<string>();
+
+tareas.Add("Estudiar C#");
+tareas.Add("Practicas bucles");
+tareas.Add("Hacer ejercicios");
+
+foreach (string tarea in tareas)
+{
+    Console.WriteLine($"📌 {tarea}");
+}
